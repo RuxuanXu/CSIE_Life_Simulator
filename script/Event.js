49c6bed98f605b011dffa59978f7f75d94535462
Ruxuan Xu <norzalamuska@gmail.com>
2018-05-22 18:19:55 +0800

    add class

---
+++ script/Event.js
@@ -0,0 +1,10 @@
+class Event {
+
+    constructor() {
+
+        //Private Members
+
+        //Public Methods
+        
+    }
+}
---
+++ script/Skill.js
@@ -0,0 +1,19 @@
+class Skill {
+
+    constructor() {
+
+        //Private Members
+        var name = '';
+        var level = 0;
+
+        //Public Methods
+        this.getName = function() {
+            return name;
+        }
+
+        this.getLevel = function(){
+            return level;
+        }
+        
+    }
+}
