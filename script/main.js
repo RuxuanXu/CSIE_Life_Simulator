eb8515c63cd2f6c9d28ec252ae8e943dac82c802
RuxuanXu <norzalamuska@gmail.com>
2018-06-07 18:39:07 +0800

    disable scale

--- script/main.js
+++ script/main.js
@@ -9,10 +9,10 @@ function resizeWindow() {
     document.getElementById("game").style.height = "100%";
     var height = $('body').height();
     var width = $('body').width();
-    if (width < $('#game').height() * 0.55) {
+    if (width < $('#game').height() * 0.6) {
         document.getElementById("game").style.width = "100%";
     } else {
-        document.getElementById("game").style.width = $('#game').height() * 0.55 + "px";
+        document.getElementById("game").style.width = $('#game').height() * 0.6 + "px";
     }
 }
 
